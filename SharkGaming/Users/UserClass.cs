813c53c3c27afc6256cd15d8245fb43f7e2078cc
Canty <chri38eb@edu.zealand.dk>
2022-12-05 15:49:47 +0100

    Update UserClass

--- SharkGaming/Users/UserClass.cs
+++ SharkGaming/Users/UserClass.cs
@@ -2,13 +2,21 @@
 {
     public class UserClass
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        private int nextId = 0;
+        protected int Id { get; set; }
+        protected string Username { get; set; }
+        protected string Password { get; set; }
 
         public UserClass()
         {
 
+        }
+
+        public UserClass(string username, string password)
+        {
+            Id = nextId++;
+            Username = username;
+            Password = password;
         }
     }
 }
