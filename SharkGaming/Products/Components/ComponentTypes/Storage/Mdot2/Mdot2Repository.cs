b7114602c0bf09d570df2e623049dae13094e183
Mille Nikita Alstrøm <mill9540@edu.zealand.dk>
2022-12-05 14:09:15 +0100

    Changes to storage classes - Lists have been added to repositories + added missing _ to CPUWaterCoolingRepository list.

--- SharkGaming/Products/Components/ComponentTypes/Storage/Mdot2/Mdot2Repository.cs
+++ SharkGaming/Products/Components/ComponentTypes/Storage/Mdot2/Mdot2Repository.cs
@@ -1,6 +1,9 @@
-﻿namespace SharkGaming.Products.Components.ComponentTypes.Storage.Mdot2
+﻿using SharkGaming.Products.Components.ComponentTypes.Cooling.CaseFan;
+
+namespace SharkGaming.Products.Components.ComponentTypes.Storage.Mdot2
 {
     public class Mdot2Repository
     {
+        public static List<Mdot2> _mdot2;
     }
 }
--- SharkGaming/Products/Components/ComponentTypes/Storage/SolidStateDrive/SolidStateDriveRepository.cs
+++ SharkGaming/Products/Components/ComponentTypes/Storage/SolidStateDrive/SolidStateDriveRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace SharkGaming.Products.Components.ComponentTypes.Storage.SolidStateDrive
+﻿using SharkGaming.Products.Components.ComponentTypes.Cooling.CaseFan;
+
+namespace SharkGaming.Products.Components.ComponentTypes.Storage.SolidStateDrive
 {
     public class SolidStateDriveRepository
     {
+        public static List<SolidStateDrive> _solidStateDrive;
     }
 }
