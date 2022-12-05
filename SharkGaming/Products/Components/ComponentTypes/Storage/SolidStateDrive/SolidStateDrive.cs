fd44ce4f4cb05abe2a9a2c4f41693f553534fdf9
Mille Nikita Alstrøm <mill9540@edu.zealand.dk>
2022-12-05 13:56:01 +0100

    Changes to storage classes - properties and empty constructors have been added.

--- SharkGaming/Products/Components/ComponentTypes/Storage/SolidStateDrive/SolidStateDrive.cs
+++ SharkGaming/Products/Components/ComponentTypes/Storage/SolidStateDrive/SolidStateDrive.cs
@@ -1,6 +1,15 @@
 ﻿namespace SharkGaming.Products.Components.ComponentTypes.Storage.SolidStateDrive
 {
-    public class SolidStateDrive
+    public class SolidStateDrive : StorageClass
     {
+        public double DriveTransferRate { get; set; }
+        public double FourKBRandomRead { get; set; }
+        public double FourKBRandomWrite { get; set; }
+        public double NANDFlashMemoryType { get; set; }
+
+        public SolidStateDrive()
+        {
+
+        }
     }
 }
