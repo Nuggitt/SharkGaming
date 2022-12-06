3dcefc718defdea2d6812fdb4543ae55c746fc9a
Nuggitt <Phulip.nielsen@gmail.com>
2022-12-06 16:03:15 +0100

    Created MOCKCPUWATERCOOLING

--- SharkGaming/MockData/MProducts/MComponents/MComponentTypes/MCooling/MockCPUCooling.cs
+++ SharkGaming/MockData/MProducts/MComponents/MComponentTypes/MCooling/MockCPUCooling.cs
@@ -1,7 +1,14 @@
-﻿namespace SharkGaming.MockData.MProducts.MComponents.MComponentTypes.MCooling
+﻿using SharkGaming.Products.Components.ComponentTypes.Cooling;
+
+namespace SharkGaming.MockData.MProducts.MComponents.MComponentTypes.MCooling
 {
     public class MockCPUCooling
     {
+        public static List<CPUCooling> _cPUCoolingList = new List<CPUCooling>();
 
+        public static List<CPUCooling> GetMockCPUCooling()
+        {
+            return _cPUCoolingList;
+        }
     }
 }
