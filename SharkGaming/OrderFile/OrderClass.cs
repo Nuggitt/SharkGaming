fd4885f9369e7ded70c76c034f0c64a009843372
Nuggitt <Phulip.nielsen@gmail.com>
2022-12-06 10:10:07 +0100

    Updated OrderClass

--- SharkGaming/OrderFile/OrderClass.cs
+++ SharkGaming/OrderFile/OrderClass.cs
@@ -1,11 +1,27 @@
-﻿namespace SharkGaming.Order
+﻿using SharkGaming.OrderFile;
+using SharkGaming.Users.Customer;
+
+namespace SharkGaming.Order
 {
     public class OrderClass
     {
-        public int id { get; set; }
-        public string deliveryAddress { get; set; }
-        public DateOnly date { get; set; }
-        public DateTime time { get; set; }
+        public static int nextId = 0;
+        public int Id { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateOnly Date { get; set; }
+        public DateTime Time { get; set; }
+        public CustomerClass Customer { get; set; }
+
+        private List<OrderItemsClass> orderItems = new List<OrderItemsClass>();
+
+        public OrderClass(string deliveryAdress, DateOnly date, DateTime time, CustomerClass customer)
+        {
+            Id = nextId++;
+            DeliveryAddress = deliveryAdress;
+            Date = date;
+            Time = time;
+            Customer = customer;
+        }
 
         public OrderClass()
         {
