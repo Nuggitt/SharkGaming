d2d68cfd40a8e1f5febcf764ca5d61daa924b291
Nuggitt <Phulip.nielsen@gmail.com>
2022-12-06 14:53:23 +0100

    MockCaseFan Created

--- SharkGaming/Products/Components/ComponentTypes/Cooling/CaseFan.cs
+++ SharkGaming/Products/Components/ComponentTypes/Cooling/CaseFan.cs
@@ -4,15 +4,17 @@
     {
         public string BearingType { get; set; }
 
-        public CaseFan()
-        {
-
-        }
 
         public CaseFan(string name, double price, bool amdCompatible, bool intelCompatible, string specInfo, string bearingType, double thermalDesignPower, double size, string powerConnector, string airFlow, double rpm, string squareMetersPerHour, string mmH20, string colourCatagory)
         : base(thermalDesignPower, size, powerConnector, airFlow, rpm, squareMetersPerHour, mmH20, colourCatagory, name, price, amdCompatible, intelCompatible, specInfo)
         {
+
             BearingType = bearingType;
+        }
+
+        public CaseFan()
+        {
+
         }
     }
 }
