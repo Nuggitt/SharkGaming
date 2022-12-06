6b5e420ec3eeb709ade8e65da386fb53abbedf77
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-06 14:01:13 +0100

    using bug fix

--- SharkGaming/MockData/MProducts/MComponents/MComponentTypes/MStorage/MockSolidStateDrive.cs
+++ SharkGaming/MockData/MProducts/MComponents/MComponentTypes/MStorage/MockSolidStateDrive.cs
@@ -1,10 +1,10 @@
-﻿using SharkGaming.Products.Components.ComponentTypes.Storage.SolidStateDrive;
+﻿using SharkGaming.Products.Components.ComponentTypes.Storage;
 
 namespace SharkGaming.MockData.MProducts.MComponents.MComponentTypes.MStorage
 {
     public class MockSolidStateDrive
     {
-        public static List<SolidStateDrive> _SoildSateDrives = new List<SolidStateDrive>()
+        public static List<SolidStateDrive> _SoildSateDrives = new List<SolidStateDrive>
         {
             new SolidStateDrive("Samsung 870 QVO 2.5¨ SSD - 1TB", 618, true, true, "Low power DDR4 SDRAM", 1000, "2.5¨", 560, 530, 11000, 35000, "Quad-niveau celle"),
             new SolidStateDrive("Samsung 870 QVO 2.5¨ SSD - 2TB", 1249, true, true, "Low power DDR4 SDRAM", 2000, "2.5¨", 560, 530, 11000, 35000, "Quad-niveau celle"),
