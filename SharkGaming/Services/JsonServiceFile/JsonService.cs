fa0f9b4dd4b8c4c0cb1fd874f71bc77d3b1d08be
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-06 10:50:44 +0100

    startede up Json, data mappe

--- SharkGaming/Services/JsonServiceFile/JsonService.cs
+++ SharkGaming/Services/JsonServiceFile/JsonService.cs
@@ -2,5 +2,19 @@
 {
     public class JsonService
     {
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public JsonService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "Componets.json"); }
+        }
+
+
     }
 }
