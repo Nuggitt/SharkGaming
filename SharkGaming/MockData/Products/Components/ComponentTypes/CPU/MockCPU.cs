eab983fa6c256efad8375bf32334302b4d8be8a6
Nuggitt <Phulip.nielsen@gmail.com>
2022-12-06 13:08:31 +0100

    Added alot of comments

--- SharkGaming/MockData/Products/Components/ComponentTypes/CPU/MockCPU.cs
+++ SharkGaming/MockData/Products/Components/ComponentTypes/CPU/MockCPU.cs
@@ -1,6 +1,12 @@
-﻿namespace SharkGaming.MockData.Products.Components.ComponentTypes.CPU
+﻿using SharkGaming.Products.Components.ComponentTypes.CPU;
+
+namespace SharkGaming.MockData.Products.Components.ComponentTypes.CPU
 {
     public class MockCPU
     {
+        //public static List<CPU> _cPUs = new List<CPU>()
+        //{
+
+        //};
     }
 }
