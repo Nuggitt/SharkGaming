902e8a334ade8d405cb848f732100f5aed7b8c6f
c-canty <christian_canty@hotmail.co.uk>
2022-12-06 10:04:39 +0100

    Added to IProductService

--- SharkGaming/Services/ProductServiceFile/IProductService.cs
+++ SharkGaming/Services/ProductServiceFile/IProductService.cs
@@ -1,6 +1,28 @@
-﻿namespace SharkGaming.Services.ProductServiceFile
+﻿using SharkGaming.Products;
+using SharkGaming.Products.Components;
+using SharkGaming.Products.Components.ComponentTypes.Cooling;
+using SharkGaming.Products.Components.ComponentTypes.Cooling.CaseFan;
+using SharkGaming.Products.Components.ComponentTypes.Cooling.CPUCooling;
+using SharkGaming.Products.Components.ComponentTypes.Cooling.CPUCooling.CPUAirCooling;
+using SharkGaming.Products.Components.ComponentTypes.Cooling.CPUCooling.CPUWaterCooling;
+using SharkGaming.Products.Components.ComponentTypes.Storage.Case;
+using SharkGaming.Products.PreBuilds;
+
+namespace SharkGaming.Services.ProductServiceFile
 {
     public interface IProductService
     {
+        List<ProductsClass> GetAllProducts();
+        List<ComponentsClass> GetAllComponents();
+        List<PreBuildsClass> GetPreBuilds();
+        List<Cases> GetCases();
+        List<CoolingClass> GetAllCooling();
+        List<CaseFan> GetCaseFans();
+        List<CPUCooling> GetCPUCooling();
+        List<CPUAirCooling> GetCPUAirCooling();
+        List<CPUWaterCooling> GetCPUWaterCooling();
+
+
+
     }
 }
