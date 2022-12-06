fffa49812d07cf0596ce272a0877dc50a8bd7191
c-canty <christian_canty@hotmail.co.uk>
2022-12-06 10:17:18 +0100

    Get (All product classes) added to IProductService

--- SharkGaming/Services/ProductServiceFile/IProductService.cs
+++ SharkGaming/Services/ProductServiceFile/IProductService.cs
@@ -5,7 +5,16 @@ using SharkGaming.Products.Components.ComponentTypes.Cooling.CaseFan;
 using SharkGaming.Products.Components.ComponentTypes.Cooling.CPUCooling;
 using SharkGaming.Products.Components.ComponentTypes.Cooling.CPUCooling.CPUAirCooling;
 using SharkGaming.Products.Components.ComponentTypes.Cooling.CPUCooling.CPUWaterCooling;
+using SharkGaming.Products.Components.ComponentTypes.CPU;
+using SharkGaming.Products.Components.ComponentTypes.GPU;
+using SharkGaming.Products.Components.ComponentTypes.Motherboard;
+using SharkGaming.Products.Components.ComponentTypes.PowerSupply;
+using SharkGaming.Products.Components.ComponentTypes.RAM;
+using SharkGaming.Products.Components.ComponentTypes.Storage;
 using SharkGaming.Products.Components.ComponentTypes.Storage.Case;
+using SharkGaming.Products.Components.ComponentTypes.Storage.Mdot2;
+using SharkGaming.Products.Components.ComponentTypes.Storage.SolidStateDrive;
+using SharkGaming.Products.CustomPc;
 using SharkGaming.Products.PreBuilds;
 
 namespace SharkGaming.Services.ProductServiceFile
@@ -15,12 +24,23 @@ namespace SharkGaming.Services.ProductServiceFile
         List<ProductsClass> GetAllProducts();
         List<ComponentsClass> GetAllComponents();
         List<PreBuildsClass> GetPreBuilds();
+        List<CustomPcClass> GetCustomPc();
         List<Cases> GetCases();
         List<CoolingClass> GetAllCooling();
         List<CaseFan> GetCaseFans();
         List<CPUCooling> GetCPUCooling();
         List<CPUAirCooling> GetCPUAirCooling();
         List<CPUWaterCooling> GetCPUWaterCooling();
+        List<CPU> GetCPUs();
+        List<GPU> GetGPUs();
+        List<Motherboard> GetMotherboards();
+        List<PowerSupply> GetPowerSupply();
+        List<RAM> GetRAM();
+        List<StorageClass> GetStorage();
+        List<Mdot2> GetMdot2();
+        List<SolidStateDrive> GetSolidStateDrives();
+
+
 
 
 
