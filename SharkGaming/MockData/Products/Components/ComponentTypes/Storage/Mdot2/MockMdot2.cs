94a309ca95203693ca9ccb8411a02cecf04d38b0
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-06 12:53:37 +0100

    New RAM

--- SharkGaming/MockData/Products/Components/ComponentTypes/Storage/Mdot2/MockMdot2.cs
+++ SharkGaming/MockData/Products/Components/ComponentTypes/Storage/Mdot2/MockMdot2.cs
@@ -1,10 +1,13 @@
-﻿using SharkGaming.Products.Components.ComponentTypes.Storage;
+﻿
+
 using SharkGaming.Products.Components.ComponentTypes.Storage.Mdot2;
 
 namespace SharkGaming.MockData.Products.Components.ComponentTypes.Storage.Mdot2
 {
     public class MockMdot2
     {
+
+
 
     }
 }
