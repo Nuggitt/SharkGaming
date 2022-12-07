0882a38bf6345fcc0fcc78bf7eeeaad0784f54fb
Mikkel Christensen <mikk060c@edu.zealand.dk>
2022-12-07 15:53:32 +0100

    Trying to make a double row navbar

---
+++ SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CasePage1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SharkGaming.Pages.ProductInfoPages.ComponentInfoPages
+{
+    public class CasePage1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
