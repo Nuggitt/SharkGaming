a700197a8801a6ba3cc92bb63d4c3e551bff841a
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-07 16:44:18 +0100

    Calculation method for prebuild and prebuild build

--- SharkGaming/MockData/MProducts/MPreBuilds/MockPreBuilds.cs
+++ SharkGaming/MockData/MProducts/MPreBuilds/MockPreBuilds.cs
@@ -1,6 +1,18 @@
-﻿namespace SharkGaming.MockData.Products.PreBuilds
+﻿using SharkGaming.Products.PreBuilds;
+
+namespace SharkGaming.MockData.Products.PreBuilds
 {
     public class MockPreBuilds
     {
+        public static List<PreBuildsClass> _preBuildsClasses = new List<PreBuildsClass>()
+        {
+            new PreBuildsClass("Dicklodon", ),
+
+        };
+
+        PreBuildsClass PreBuildsClass01 = new PreBuildsClass("Dickledon", 0, "Big dick pc for the big DICK GAMER",  );
+
+        public static List<PreBuildsClass> GetMockPreBuilds() { return _preBuildsClasses; }
+
     }
 }
