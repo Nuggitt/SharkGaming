7df530601a487e0f027e1a4b971f3a73afcd1d1c
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-07 12:38:17 +0100

    Test MockDATA

--- SharkGaming/Pages/TestSite/TestRoom.cshtml.cs
+++ SharkGaming/Pages/TestSite/TestRoom.cshtml.cs
@@ -1,7 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SharkGaming.MockData.Products.Components.ComponentTypes.Ram;
 using SharkGaming.Products.Components;
+using SharkGaming.Products.Components.ComponentTypes.Cooling;
+using SharkGaming.Products.Components.ComponentTypes.Motherboard;
+using SharkGaming.Products.Components.ComponentTypes.RAM;
+using SharkGaming.Services.ProductServiceFile;
 using System.Runtime.CompilerServices;
 
 namespace SharkGaming.Pages.TestSite
@@ -10,16 +15,20 @@ namespace SharkGaming.Pages.TestSite
     {
 
 
-        public static List<ComponentsClass> components = new List<ComponentsClass>()
+        public static List<CaseFan> component = new List<CaseFan>()
         {
-            new ComponentsClass("Asus 570XX", 12999, true, false, "Very gut"),
-            new ComponentsClass("Asus 590XX", 15999, true, false, "Very gut")
+        };
+
+        public static List<RAM> components = new List<RAM>()
+        {
         };
 
         //TESTING NEXT ID NEVER TOUCH IT AGAIN
 
         public void OnGet()
         {
+            component = MockData.MProducts.MComponents.MComponentTypes.MCooling.MockCaseFan.GetMockCaseFans();
+            components = MockRam.GetMockRam();
 
         }
 
