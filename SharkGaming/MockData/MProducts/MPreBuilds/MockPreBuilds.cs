9f48fcecdcac6fd33f6a6b2f7ce81ea030db6cbc
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-07 16:48:11 +0100

    bug fix

--- SharkGaming/MockData/MProducts/MPreBuilds/MockPreBuilds.cs
+++ SharkGaming/MockData/MProducts/MPreBuilds/MockPreBuilds.cs
@@ -6,11 +6,11 @@ namespace SharkGaming.MockData.Products.PreBuilds
     {
         public static List<PreBuildsClass> _preBuildsClasses = new List<PreBuildsClass>()
         {
-            new PreBuildsClass("Dicklodon", ),
+            //new PreBuildsClass("Dicklodon", ),
 
         };
 
-        PreBuildsClass PreBuildsClass01 = new PreBuildsClass("Dickledon", 0, "Big dick pc for the big DICK GAMER",  );
+        //PreBuildsClass PreBuildsClass01 = new PreBuildsClass("Dickledon", 0, "Big dick pc for the big DICK GAMER", true, );
 
         public static List<PreBuildsClass> GetMockPreBuilds() { return _preBuildsClasses; }
 
