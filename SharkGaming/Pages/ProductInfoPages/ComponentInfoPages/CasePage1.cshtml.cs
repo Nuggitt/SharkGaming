47be05771850efba7ef2cb209d368faaf187122f
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-08 11:38:54 +0100

    Test til case

--- SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CasePage1.cshtml.cs
+++ SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CasePage1.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SharkGaming.Products.Components.ComponentTypes.RAM;
+using SharkGaming.Services.ProductServiceFile;
 
 namespace SharkGaming.Pages.ProductInfoPages.ComponentInfoPages
 {
     public class CasePage1Model : PageModel
     {
+
+        private IProductService _productService;
+
+        public CasePage1Model(IProductService iproductervice)
+        {
+            _productService = iproductervice;
+        }
+        public List<RAM>? components { get; set; }
+
         public void OnGet()
         {
+            components = _productService.GetRAM();
         }
     }
 }
