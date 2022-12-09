8dc7813834fff25ea110043b3bb585d88ce10e6c
Canty <chri38eb@edu.zealand.dk>
2022-12-09 19:14:46 +0100

    MockOrderItemsClass created

--- SharkGaming/OrderFile/OrderClass.cs
+++ SharkGaming/OrderFile/OrderClass.cs
@@ -8,21 +8,19 @@ namespace SharkGaming.Order
         public static int nextId = 0;
         public int Id { get; set; }
         public string DeliveryAddress { get; set; }
-        public DateOnly Date { get; set; }
-        public DateTime Time { get; set; }
+        public DateTime Dt { get; set; }
         public CustomerClass Customer { get; set; }
         public double TotalPrice { get; set; }
 
         private List<OrderItemsClass> _orderItems = new List<OrderItemsClass>();
         private static List<OrderClass> _orders = new List<OrderClass>();
 
-        public OrderClass(CustomerClass customer, string deliveryAdress, DateOnly date, DateTime time, List<OrderItemsClass> orderitems)
+        public OrderClass(CustomerClass customer, string deliveryAdress, List<OrderItemsClass> orderitems)
         {
             Id = nextId++;
             Customer = customer;
             DeliveryAddress = deliveryAdress;
-            Date = date;
-            Time = time;
+            Dt = DateTime.Now;
             _orderItems = orderitems;
             TotalPrice = CalculateTotalPrice();
             _orders.Add(this);
