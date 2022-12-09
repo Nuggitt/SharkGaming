717c291b44661d593bf1057128da7d09e43bcd1e
Mikkel Christensen <mikk060c@edu.zealand.dk>
2022-12-09 10:54:13 +0100

    Made CasePage2

--- SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CasePage2.cshtml.cs
+++ SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CasePage2.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SharkGaming.Products.Components.ComponentTypes.RAM;
+using SharkGaming.Products.Components.ComponentTypes.Storage.Case;
+using SharkGaming.Services.ProductServiceFile;
 
 namespace SharkGaming.Pages.ProductInfoPages.ComponentInfoPages
 {
-    public class CasePage2Model : PageModel
-    {
-        public void OnGet()
+        public class CasePage2Model : PageModel
         {
+
+            private IProductService _productService;
+
+            public CasePage2Model(IProductService iproductervice)
+            {
+                _productService = iproductervice;
+            }
+            public List<Cases>? components { get; private set; }
+
+
+            public void OnGet()
+            {
+                components = _productService.GetCases();
+            }
         }
-    }
 }
