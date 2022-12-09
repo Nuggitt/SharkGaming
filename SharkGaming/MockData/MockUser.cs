d2ee51b19c3bfead123ad2151f673fbbdd47033e
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2022-12-09 11:04:16 +0100

    Mock User set up

---
+++ SharkGaming/MockData/MockUser.cs
@@ -0,0 +1,19 @@
+﻿using SharkGaming.Users;
+
+namespace SharkGaming.MockData
+{
+    public class MockUser
+    {
+
+        public static List<UserClass> userClasses = new List<UserClass>()
+        {
+            new UserClass("admin", "1234"),
+            new UserClass("phillip", "1234"),
+            new UserClass("canty", "1234"),
+            new UserClass("mikkel", "1234"),
+            new UserClass("mille", "1234")
+        };
+
+
+    }
+}
