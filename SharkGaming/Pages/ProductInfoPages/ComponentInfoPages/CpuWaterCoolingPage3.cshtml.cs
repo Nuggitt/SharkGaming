b334b4b6cac07afb9ac8bce4c91aeef0ed583cc7
Mikkel Christensen <mikk060c@edu.zealand.dk>
2022-12-09 15:58:56 +0100

    CPU water cooling 1-3 is done

--- SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CpuWaterCoolingPage3.cshtml.cs
+++ SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/CpuWaterCoolingPage3.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SharkGaming.Services.ProductServiceFile;
 
 namespace SharkGaming.Pages.ProductInfoPages.ComponentInfoPages
 {
     public class CpuWaterCoolingPage3Model : PageModel
     {
+        private IProductService _productService;
+
+        public CpuWaterCoolingPage3Model(IProductService iproductervice)
+        {
+            _productService = iproductervice;
+        }
+        public List<Products.Components.ComponentTypes.Cooling.CPUWaterCooling>? components { get; private set; }
+
         public void OnGet()
         {
+            components = _productService.GetCPUWaterCooling();
         }
     }
 }
