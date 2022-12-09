ac65b0b610bcdcdf68e1d99d0df045ec675d4769
Nuggitt <Phulip.nielsen@gmail.com>
2022-12-09 14:41:30 +0100

    Added shoppingcart

---
+++ SharkGaming/Pages/OrderInfoPages/OrderItemsPage.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SharkGaming.Products;
+using SharkGaming.Services.OrderRepositoryServiceFile;
+using SharkGaming.Services.ProductServiceFile;
+using System.Reflection;
+
+namespace SharkGaming.Pages.OrderInfoPages
+{
+    public class OrderItemsPageModel : PageModel
+    {
+        //private IOrderRepositoryService _orderService;
+
+
+        //public OrderItemsPageModel(OrderRepositoryService orderService)
+        //{
+        //    _orderService = orderService;
+        //}
+        //public List<> _productsadded { get; private set; } 
+        
+        //public void OnGet()
+        //{
+        //    _productsadded = _productService.GetOrderItems();
+        //}
+
+
+
+    }
+}
