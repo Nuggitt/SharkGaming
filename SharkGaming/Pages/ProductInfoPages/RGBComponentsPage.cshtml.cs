1d491a34ded00a3bbe95b91fa24ffed85a348782
Canty <chri38eb@edu.zealand.dk>
2022-12-12 12:42:25 +0100

    RGB

---
+++ SharkGaming/Pages/ProductInfoPages/RGBComponentsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SharkGaming.Pages.ProductInfoPages
+{
+    public class RGBComponentsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
