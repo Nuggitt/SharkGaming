38d564062460dec23a8729cb3182bca0fadb49e6
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2022-12-14 16:58:46 +0100

    Motherboards

--- SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/MotherBoardPage1.cshtml.cs
+++ SharkGaming/Pages/ProductInfoPages/ComponentInfoPages/MotherBoardPage1.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SharkGaming.Products.Components.ComponentTypes.Motherboard;
 using SharkGaming.Products.Components.ComponentTypes.Storage;
+using SharkGaming.Services.OrderRepositoryServiceFile;
 using SharkGaming.Services.ProductServiceFile;
 
 namespace SharkGaming.Pages.ProductInfoPages.ComponentInfoPages
@@ -9,16 +10,28 @@ namespace SharkGaming.Pages.ProductInfoPages.ComponentInfoPages
     public class MotherBoardPage1Model : PageModel
     {
         private IProductService _productService;
+        private IOrderRepositoryService _orderService;
+        [BindProperty] public int productId { get; set; }
+        [BindProperty] public int amount { get; set; }
+        [BindProperty] public double price { get; set; }
 
-        public MotherBoardPage1Model(IProductService iproductervice)
+        public MotherBoardPage1Model(IProductService productService, IOrderRepositoryService orderService)
         {
-            _productService = iproductervice;
+            this._productService = productService;
+            this._orderService = orderService;
+
         }
         public List<Motherboard>? motherboards { get; private set; }
 
         public void OnGet()
         {
             motherboards = _productService.GetMotherboards();
+        }
+
+        public IActionResult OnPostAddToCart()
+        {
+            _orderService.AddToCart(18, 1, 2932.32);
+            return RedirectToPage("MotherBoardPage1");
         }
     }
 }
