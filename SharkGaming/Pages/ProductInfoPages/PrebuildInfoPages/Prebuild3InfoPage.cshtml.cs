0bdb8a7480577c0b73378ecb1c2d9a0610e4e29f
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2022-12-14 18:08:32 +0100

    Prebuilds done

--- SharkGaming/Pages/ProductInfoPages/PrebuildInfoPages/Prebuild3InfoPage.cshtml.cs
+++ SharkGaming/Pages/ProductInfoPages/PrebuildInfoPages/Prebuild3InfoPage.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SharkGaming.Products.PreBuilds;
+using SharkGaming.Services.OrderRepositoryServiceFile;
 using SharkGaming.Services.ProductServiceFile;
 
 namespace SharkGaming.Pages.ProductInfoPages.PrebuildInfoPages
@@ -8,10 +9,16 @@ namespace SharkGaming.Pages.ProductInfoPages.PrebuildInfoPages
     public class Prebuild3InfoPageModel : PageModel
     {
         private IProductService _productService;
+        private IOrderRepositoryService _orderService;
+        [BindProperty] public int productId { get; set; }
+        [BindProperty] public int amount { get; set; }
+        [BindProperty] public double price { get; set; }
 
-        public Prebuild3InfoPageModel(IProductService iproductervice)
+        public Prebuild3InfoPageModel(IProductService productService, IOrderRepositoryService orderService)
         {
-            _productService = iproductervice;
+            this._productService = productService;
+            this._orderService = orderService;
+
         }
         public List<PreBuildsClass>? preBuilds { get; private set; }
 
@@ -19,6 +26,14 @@ namespace SharkGaming.Pages.ProductInfoPages.PrebuildInfoPages
         public void OnGet()
         {
             preBuilds = _productService.GetPreBuilds();
+        }
+
+        public IActionResult OnPostAddToCart()
+        {
+
+            _orderService.AddToCart(35, 1, 9999);
+            return RedirectToPage("Prebuild3InfoPage");
+
         }
     }
 }
