1f2333a63e2a12eebfe84f04f18599b1be09aebd
Nuggitt <Phulip.nielsen@gmail.com>
2022-12-21 05:53:54 +0100

    Updated Prebuilds add to cart from prebuildspage

--- SharkGaming/Pages/ProductInfoPages/PreBuildListPage.cshtml.cs
+++ SharkGaming/Pages/ProductInfoPages/PreBuildListPage.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SharkGaming.Products;
 using SharkGaming.Products.PreBuilds;
+using SharkGaming.Services.OrderRepositoryServiceFile;
 using SharkGaming.Services.ProductServiceFile;
 
 namespace SharkGaming.Pages.ProductInfoPages
@@ -9,10 +11,15 @@ namespace SharkGaming.Pages.ProductInfoPages
     {
         public string img = "/Images/Components/Case1.png";
         private IProductService _productService;
+        private IOrderRepositoryService _orderRepositoryService;
+        [BindProperty] public int productId { get; set; }
+        [BindProperty] public int amount { get; set; }
+        [BindProperty] public double price { get; set; }
 
-        public PreBuildListPageModel(IProductService iproductervice)
+        public PreBuildListPageModel(IProductService iproductervice, IOrderRepositoryService iorderRepositoryService )
         {
             _productService = iproductervice;
+            _orderRepositoryService = iorderRepositoryService;
         }
         public List<PreBuildsClass>? preBuilds { get; private set; }
 
@@ -20,6 +27,16 @@ namespace SharkGaming.Pages.ProductInfoPages
         public void OnGet()
         {
             preBuilds = _productService.GetPreBuilds();
+        }
+
+        public IActionResult OnPostAddToCart(int id)
+        {
+            productId = id;
+
+            _orderRepositoryService.AddToCart(productId, 1, price);
+            return RedirectToPage("PreBuildListPage");
+
+
         }
     }
 }
